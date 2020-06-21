6a2c5df03facc3a66196c6213460c4e65a46c8b1
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-06-20 21:05:11 -0400

    logout/requires-auth middelware functionality done

---
+++ auth/requires-auth.js
@@ -0,0 +1,9 @@
+//middleware
+module.exports=(req,res,next) => {
+    const logged = req.session.user;
+    if(logged){
+        next()
+    } else {
+        res.status(401).json({message: "You must login first before accessing the schedules!"})
+    }}
+  
