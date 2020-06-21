98dc528d1a7c9a4cfccc9fa5a3a3677727737a23
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-06-20 20:55:42 -0400

    register/login functionality done

--- auth/auth-helpers.js
+++ auth/auth-helpers.js
@@ -1,5 +1,16 @@
 const db = require('../data/db-config');
 
+const register = (credentials) => {
+    return db('teacher')
+    .insert(credentials)
+    .select("teacher.username")
+}
 
+const login = (dataMatch) => {
+    return db("teacher").where(dataMatch).orderBy("id");
+  }
 
-module.exports = {}
+module.exports = {
+    register,
+    login
+}
