64e299e0a2b91d51aabcf8fdc8de51b07f087714
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-06-21 15:29:10 -0400

    fixed starts script

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('./server.js');
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8001;
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
 });
