1b59b0993e1d8547ed5b28f9e9fff968c08d74ea
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-06-22 19:10:35 -0400

    pro add

--- knexfile.js
+++ knexfile.js
@@ -1,38 +1,55 @@
-
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     useNullAsDefault: true,
     connection: {
-      filename: './BetterProfessor.sqlite3.db3'
+      filename: "./BetterProfessor.sqlite3.db3",
     },
     migrations: {
-      directory: './data/migrations'
+      directory: "./data/migrations",
     },
     seeds: {
-      directory: './data/seeds'
+      directory: "./data/seeds",
     },
     pool: {
       afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done)
+        conn.run("PRAGMA foreign_keys = ON", done);
       },
+    },
   },
- }
-}
+  production: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./BetterProfessor.sqlite3.db3",
+    },
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+  },
+};
 
-  //for postgres
+//for postgres
 
- // production: {
-  //   client: "pg",
-  //   connection: process.env.DATABASE_URL,
-  //     pool: {
-  //       min: 2,
-  //       max: 10,
-  //     },
-  //   migrations: {
-  //     directory: "./data/migrations",
-  //   },
-  //   seeds: {
-  //     directory: "./data/seeds",
-  //   },
-  // },
+// production: {
+//   client: "pg",
+//   connection: process.env.DATABASE_URL,
+//     pool: {
+//       min: 2,
+//       max: 10,
+//     },
+//   migrations: {
+//     directory: "./data/migrations",
+//   },
+//   seeds: {
+//     directory: "./data/seeds",
+//   },
+// },
