038d0ad4cd2791b0db44114c02444b58a6601957
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-06-22 12:57:53 -0400

    test for launch

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // const knex = require('knex')('production')
 // require('dotenv').config()
 const server = require('./server.js');
+require('dotenv').config()
 const PORT = process.env.PORT || 8001;
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
