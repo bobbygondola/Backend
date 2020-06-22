4127d79fce9aa28d109c04eeaa60fe37aa54c050
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-06-22 13:48:39 -0400

    depo test 4

--- knexfile.js
+++ knexfile.js
@@ -21,20 +21,22 @@ module.exports = {
         conn.run('PRAGMA foreign_keys = ON', done)
       },
   },
+ }
+}
 
-  production: {
-    client: "pg",
-    connection: process.env.DATABASE_URL,
-      pool: {
-        min: 2,
-        max: 10,
-      },
-    migrations: {
-      directory: "./data/migrations",
-    },
-    seeds: {
-      directory: "./data/seeds",
-    },
-  },
-}
-}
+  //for postgres
+
+ // production: {
+  //   client: "pg",
+  //   connection: process.env.DATABASE_URL,
+  //     pool: {
+  //       min: 2,
+  //       max: 10,
+  //     },
+  //   migrations: {
+  //     directory: "./data/migrations",
+  //   },
+  //   seeds: {
+  //     directory: "./data/seeds",
+  //   },
+  // },
