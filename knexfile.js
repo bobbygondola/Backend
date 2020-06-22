045aa10a7019248c89ecf4ed0a9ccfa748e0e5e3
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-06-22 14:46:44 -0400

    robert-gondola

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './BetterProfessor.db3'
+      filename: './BetterProfessor.sqlite3.db3'
     },
     migrations: {
       directory: './data/migrations'
