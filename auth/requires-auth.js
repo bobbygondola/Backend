19c0c26f2a197c93da5f835297c3028fc3229cd3
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-06-23 21:04:51 -0400

    jwt token

--- auth/requires-auth.js
+++ auth/requires-auth.js
@@ -1,9 +1,28 @@
-//middleware
-module.exports=(req,res,next) => {
-    const logged = req.session.user;
-    if(logged){
-        next()
-    } else {
-        res.status(401).json({message: "You must login first before accessing the schedules!"})
-    }}
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets")
+module.exports = (req, res, next) => {
+
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (error, decodedToken) => {
+      if (error) {
+        res.status(401).json({ error: "You must be logged in to do this" });
+      } 
+      else {
+        req.jwt = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ error: "You must be logged in to do this" });
+  }
+};
   
+
+    // module.exports=(req,res,next) => {
+    //     const logged = req.session.user;
+    //     if(logged){
+    //         next()
+    //     } else {
+    //         res.status(401).json({message: "You must login first before accessing the schedules!"})
+    //     }}
