c7ea72d7244c9b804524ef4cf921647389130579
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-06-25 11:41:04 -0400

    tests and update

--- data/db-config.js
+++ data/db-config.js
@@ -3,6 +3,12 @@ const knexConfig = require('../knexfile');
 const environment = process.env.NODE_ENV;
 module.exports = knex(knexConfig[environment]);
 
+////testing
+// const knex = require('knex');
+// const knexConfig = require('../knexfile');
+// module.exports = knex(knexConfig.development);
+
+
 //connection to Database FOR PG ADMIN
 // const knex = require('knex');
 // const config = require('../knexfile');
